650db69ce558b12038516db196fcb7d1de58f50f
Tiger Nie <nhl0819@gmail.com>
2021-04-05 13:34:18 +0800

    formatting with prettier

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text h1 style={styles.h1_text}>NonProvDev</Text>
-      <Text h3 style={styles.h3_text}>A simple, intuitive platform to connect non-profit organizations with passionate web developers</Text>
-      <Text h3 style={styles.h3_text}>Created by the CS 394 Red Team</Text>
-      <Text h4 style={styles.h4_text}>Benjamin Nober, Chris Song, Jacob Wat, Linus Okoth, Mason Brachmann, Tiger Nie, Zaddeen Benaissa</Text>
+      <Text h1 style={styles.h1_text}>
+        NonProvDev
+      </Text>
+      <Text h3 style={styles.h3_text}>
+        A simple, intuitive platform to connect non-profit organizations with
+        passionate web developers
+      </Text>
+      <Text h3 style={styles.h3_text}>
+        Created by the CS 394 Red Team
+      </Text>
+      <Text h4 style={styles.h4_text}>
+        Benjamin Nober, Chris Song, Jacob Wat, Linus Okoth, Mason Brachmann,
+        Tiger Nie, Zaddeen Benaissa
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,9 +27,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   h1_text: {
     fontSize: 48,
@@ -27,13 +37,12 @@ const styles = StyleSheet.create({
   },
   h3_text: {
     fontSize: 24,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 20,
   },
   h4_text: {
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 15,
   },
-
 });
